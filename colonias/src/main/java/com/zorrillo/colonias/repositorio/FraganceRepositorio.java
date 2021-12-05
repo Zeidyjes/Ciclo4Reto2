650c6ac9f0da37c1e1945840c22e19b79848d226
Zeidyjes <zeidy.estupinan.mt@correo.usa.edu.co>
2021-12-05 16:35:29 -0500

    first commit

---
+++ colonias/src/main/java/com/zorrillo/colonias/repositorio/FraganceRepositorio.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zorrillo.colonias.repositorio;
+
+import com.zorrillo.colonias.interfaces.InterfaceFragance;
+import com.zorrillo.colonias.modelo.Fragance;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ydiez
+ */
+@Repository
+public class FraganceRepositorio {
+    @Autowired
+    private InterfaceFragance repository;
+
+    public List<Fragance> getAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Fragance> getClothe(String reference) {
+        return repository.findById(reference);
+    }
+    public Fragance create(Fragance clothe) {
+        return repository.save(clothe);
+    }
+
+    public void update(Fragance clothe) {
+        repository.save(clothe);
+    }
+    
+    public void delete(Fragance clothe) {
+        repository.delete(clothe);
+    }
+
+
+}
---
+++ colonias/src/main/java/com/zorrillo/colonias/servicio/UserFragance.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zorrillo.colonias.servicio;
+
+import com.zorrillo.colonias.modelo.Fragance;
+import com.zorrillo.colonias.repositorio.FraganceRepositorio;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Zeidy Johana Estupiñan S.
+ */
+@Service
+public class UserFragance {
+     @Autowired
+    private FraganceRepositorio clotheRepository;
+
+    public List<Fragance> getAll() {
+        return clotheRepository.getAll();
+    }
+
+   public Optional<Fragance> getClothe(String reference) {
+        return clotheRepository.getClothe(reference);
+    }
+
+    public Fragance create(Fragance accesory) {
+        if (accesory.getReference() == null) {
+            return accesory;
+        } else {
+            return clotheRepository.create(accesory);
+        }
+    }
+
+    public Fragance update(Fragance accesory) {
+
+        if (accesory.getReference() != null) {
+            Optional<Fragance> accesoryDb = clotheRepository.getClothe(accesory.getReference());
+            if (!accesoryDb.isEmpty()) {
+                
+                if (accesory.getBrand()!= null) {
+                    accesoryDb.get().setBrand(accesory.getBrand());
+                }
+                
+                if (accesory.getCategory() != null) {
+                    accesoryDb.get().setCategory(accesory.getCategory());
+                }
+                
+                if (accesory.getDescription() != null) {
+                    accesoryDb.get().setDescription(accesory.getDescription());
+                }
+                if (accesory.getPrice() != 0.0) {
+                    accesoryDb.get().setPrice(accesory.getPrice());
+                }
+                if (accesory.getQuantity() != 0) {
+                    accesoryDb.get().setQuantity(accesory.getQuantity());
+                }
+                if (accesory.getPhotography() != null) {
+                    accesoryDb.get().setPhotography(accesory.getPhotography());
+                }
+                accesoryDb.get().setAvailability(accesory.isAvailability());
+                clotheRepository.update(accesoryDb.get());
+                return accesoryDb.get();
+            } else {
+                return accesory;
+            }
+        } else {
+            return accesory;
+        }
+    }
+
+    public boolean delete(String reference) {
+        Boolean aBoolean = getClothe(reference).map(accesory -> {
+            clotheRepository.delete(accesory);
+            return true;
+        }).orElse(false);
+        return aBoolean;
+    }
+    
+
+}
